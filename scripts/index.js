c41d496ac3e01b5432ded3b7b98d8e8270c99cac
Emil Niftiev <e.niftiev@yandex.ru>
2022-12-21 17:04:35 +0300

    feat: сделать заготовку для новых поп-апов

--- scripts/index.js
+++ scripts/index.js
@@ -6,17 +6,64 @@ let profileName = document.querySelector(".profile__name");
 let profileJob = document.querySelector(".profile__job");
 let editButton = document.querySelector(".profile__edit-button");
 let closeButton = document.querySelector(".pop-up__close-button");
+const addNewImagePopup = document.querySelector(".pop-up_type_new-image");
+const addNewImageButton = document.querySelector(".profile__add-button");
+const closeButtonNewImagePopup = addNewImagePopup.querySelector(
+  ".pop-up__close-button"
+);
 
-// Открыть pop-up
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
 
-editButton.addEventListener("click", function () {
+// Открыть pop-up добавления карточки
+
+function openAddImagePopup() {
+  addNewImagePopup.classList.add("pop-up_opened");
+}
+addNewImageButton.addEventListener("click", openAddImagePopup);
+
+// Закрыть pop-up добавления карточки
+
+function closeAddImagePopup() {
+  addNewImagePopup.classList.remove("pop-up_opened");
+}
+
+closeButtonNewImagePopup.addEventListener("click", closeAddImagePopup);
+
+// Открыть pop-up редактирования профиля
+
+function openPopup() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-
   popup.classList.add("pop-up_opened");
-});
+}
+editButton.addEventListener("click", openPopup);
 
-// Закрыть pop-up
+// Закрыть pop-up редактирования профиля
 
 function closeForm() {
   popup.classList.remove("pop-up_opened");
@@ -31,5 +78,4 @@ function handleFormSubmit(evt) {
   profileJob.textContent = jobInput.value;
   closeForm();
 }
-
 formElement.addEventListener("submit", handleFormSubmit);
